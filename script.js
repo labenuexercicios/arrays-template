ba4927cb0a4630e82d1e605322d2cc0cd425a384
Raul Cesar <raulcesar.rcf@gmail.com>
2022-07-27 21:03:42 -0300

    atividade labenu

---
+++ script.js
@@ -0,0 +1,76 @@
+// const arrayStrings = ["tayanne", "silva", "Novais"]
+
+// const arrayNumbers =[0, 7, 2]
+
+// const arrayMisto = [2, "neymar", true]
+
+// const arrayUnico = [5]
+
+// const array = []
+// // 1
+// console.log(arrayStrings.length)
+
+// console.log(arrayStrings[3])
+// // array 3
+// console.log(arrayMisto.includes("neymar"))
+// // array 4
+// console.log(arrayUnico)
+
+//console.log(arrayStrings = arrayMisto)
+//console.log(arrayStrings)
+
+//const copiaArray = arrayNumbers // errado - cria uma referencia ao array original
+
+//copiaArray.push(4) //errado
+
+//Criar array - Alocar lugar na memoria pra ele
+
+//Copia da forma errada - Apontar pro lugar que esta alocado
+
+// const copiaDeVerdade = arrayNumbers.slice()
+
+// copiaDeVerdade.push(3)
+
+// console.log("Copia de verdade, ", copiaDeVerdade)
+
+// push adiciona coisas -- 
+
+// slice cria um novo array que e != do que criar uma referecia
+// do array original
+
+// copiaDeVerdade.splice(2, 2)
+//i - indice
+//n - elementos
+
+// console.log(copiaDeVerdade)
+
+// console.log(arrayStrings.sort())
+// console.log(arrayNumbers.sort())
+
+//order crescente e alfabetios
+//sort() -> comportamento padrao
+// utilizar comportamentos customizados das funcoes 
+//sort sempre olha qual numero primeiro nao como um todo
+
+
+
+//cria um copia de verdade = slice
+// quero tirar coisas do meio do array = splice 
+
+
+const exerFix = [4, 9, 8 , 1, 5]
+
+console.log(exerFix)
+
+console.log(exerFix.sort())
+
+console.log(exerFix.reverse())
+
+console.log(exerFix.pop())
+
+console.log(exerFix.push(6))
+
+console.log(exerFix.splice(2,1))
+
+console.log(exerFix)
+
