d97170baf92e9c5879e518061ba39213289cb78a
Jessé Amaral <jesseelite286@gmail.com>
2022-07-27 21:45:19 -0300

    a010 Template exercicio feito 27/07/22

---
+++ script.js
@@ -0,0 +1,20 @@
+const arrayNumber = [4, 3, 1, 2, 6, 5]
+console.log(arrayNumber)
+
+arrayNumber.sort()
+console.log(arrayNumber);
+
+arrayNumber.reverse()
+console.log(arrayNumber);
+
+arrayNumber.pop()
+console.log(arrayNumber)
+
+arrayNumber.push(6)
+console.log(arrayNumber)
+
+arrayNumber.splice(2, 1)
+
+
+console.log(arrayNumber)
+
