a03265cea343f755412820e0112e5b6bac6073a9
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-07-27 21:30:40 -0300

    exercicios prontos

---
+++ script.js
@@ -0,0 +1,19 @@
+numeros = [0,5,9,7,3,1,2];
+
+console.log ("normal",numeros);
+
+numeros.sort();
+console.log ("crescente",numeros);
+
+numeros.reverse();
+console.log ("decrescente",numeros);
+
+numeros.pop();
+numeros.push(6);
+numeros.splice(2,1);
+
+console.log("pop,push,splice",numeros);
+
+
+
+
