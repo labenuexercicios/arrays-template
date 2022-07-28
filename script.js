40124585baa00e82a7bb9a2d8b6644184225cabc
PedroHenriqueLira <pedrohenriquecardoso99@hotmail.com>
2022-07-27 21:11:06 -0300

    Atividade Arrays

---
+++ script.js
@@ -0,0 +1,52 @@
+// const nome = "pedro"
+// const nome2 = "joão"
+
+// console.log(`${nome} tem mais letras do que ${nome2}?`)
+// console.log([nome.length, nome2.length])
+// console.log(nome.length >= nome2.length )
+
+// let arrayString = ["carro","moto","aviao"]
+// let arrayNumber = [3,5,1]
+// let arrayTodos = ["mamao",1,true]
+// let arrayValor = [1]
+// let arrayVazio = []
+
+// console.log(arrayString)
+// arrayTodos.includes("mamao")
+// console.log(arrayTodos)
+// arrayTodos.includes(1)
+// console.log(arrayTodos)
+// arrayTodos.includes(true)
+// console.log(arrayTodos)
+// console.log(arrayValor)
+// console.log(arrayVazio)
+// arrayVazio.length
+// console.log(arrayVazio.length)
+
+// let arrayStringCopia = arrayString.slice()
+ 
+// console.log(arrayStringCopia)
+// arrayStringCopia.splice(1,1)
+// console.log(arrayStringCopia)
+
+
+//arrayStringCopia.sort()
+// console.log(arrayStringCopia)
+//arrayNumber.sort()
+// console.log(arrayNumber)
+
+
+
+let arrayNovo = [20,10,35,80]
+console.log(arrayNovo)
+arrayNovo.sort()
+console.log(arrayNovo)
+arrayNovo.reverse()
+console.log(arrayNovo)
+arrayNovo.pop()
+console.log(arrayNovo)
+arrayNovo.push(6)
+console.log(arrayNovo)
+arrayNovo.splice(2,1)
+console.log(arrayNovo)
+
