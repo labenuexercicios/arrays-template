0df7867d3d124323401ea7449a4fac30053dea3d
BrunaSpaniol <bruna.spaniol@gmail.com>
2022-09-07 11:12:26 -0300

    Aula sobre arrays

---
+++ script.js
@@ -0,0 +1,10 @@
+const Array = [1, 2, 80, 5, 1, 400, 2, 6];
+console.log("Array original", Array)
+
+console.log(Array.sort(), Array.reverse())
+// Array.sort().reverse()
+
+Array.pop()
+Array.push(6)
+Array.splice(2,1)
+console.log(Array)
