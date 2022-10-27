5bffd1a5f9a7bba8db0d79288fe340e033a6c850
Rafaela Guedes da Costa <rafaela.costa@meta.com.br>
2022-10-26 21:13:12 -0300

    resolucao-exercicios

---
+++ script.js
@@ -0,0 +1,38 @@
+const arrayS = ["Rafa", "28", "Casada"]
+
+const arrayN = [28, 29, 30]
+
+const arrayT = ["Rafa", 28, true]
+
+const unico = [5]
+
+const nenhum = []
+/*
+console.log(arrayS.length)
+console.log(arrayS[2])
+
+console.log(unico)
+console.log(unico.length)
+*/
+/*
+const copiaArray = arrayS.slice()
+
+console.log(copiaArray)
+copiaArray.push("Rio")
+
+console.log(copiaArray)
+copiaArray.splice(2,2)
+
+console.log(copiaArray)
+*/
+
+const array = [80, 65, 10, 3, 7, 5985]
+
+const copiaArray = array.slice()
+
+copiaArray.pop(5)
+copiaArray.push(6)
+copiaArray.splice(2,1)
+
+console.log(array)
+console.log(copiaArray)
