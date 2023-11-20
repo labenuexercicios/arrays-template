e6f6c15575fffd6be45e18bf2f50b9e2889b63b3
bruhoffman <bruhoffman@gmail.com>
2023-11-20 17:25:05 -0300

    Exercícios Aula Prática

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function imprimirArray (){
-  //digite a sua solução aqui
+//Prática Exercício 1
+/* function imprimirArray() {
+  const arrayString = ["Vitória", "Rio de Janeiro", "Belo Horizonte", "São Paulo"];
+  const arrayNumber = [1, 10, 100];
+  const arrayMult = ["São Paulo", 1000, true];
+  const arraySingle = [2];
+  const arraySemValor = [];
 
+  console.log(arrayString);
+  console.log(arrayNumber);
+  console.log(arrayMult);
+  console.log(arrayString.length);
+  console.log(arrayString[3]);
+  console.log(arraySingle, arraySingle.indexOf(2), arraySingle.length);
+} */
+
+//Prática Exercício 2
+function imprimirArray() {
+  const cidades = ["Vitória", "Rio de Janeiro", "Belo Horizonte"];
+  const cidades2 = cidades;
+
+  console.log(`Cidades: ${cidades}`);
+  console.log(`Cidades2: ${cidades2}`);
+
+  cidades.push("São Paulo");
+
+  console.log(`Cidades: ${cidades}`);
+  console.log(`Cidades2: ${cidades2}`);
+
+  const cidades3 = cidades.slice();
+  console.log(`Cidades: ${cidades}`);
+  console.log(`Cidades3: ${cidades3}`);
+
+  cidades.push("Natal");
+  console.log(`Cidades: ${cidades}`);
+  console.log(`Cidades3: ${cidades3}`);
+
+  cidades3.splice(2, 2);
+  console.log(cidades3);
 }
 
 imprimirArray()
